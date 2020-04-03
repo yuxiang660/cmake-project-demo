09d756ad2712448a6cde11ceb11c4a3ea88bf5fa
ben <yuxiang660@163.com>
2020-04-04 00:08:42 +0800

    add install

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# "set -x" expands variables and prints a little + sign before the line.
+
+SOURCE_DIR=`pwd`
+BUILD_DIR=${BUILD_DIR:-./build}
+BUILD_TYPE=${BUILD_TYPE:-Debug}
+INSTALL_DIR=${INSTALL_DIR:-./export}
+CXX=${CXX:-g++}
+
+mkdir -p $BUILD_DIR \
+  && cd $BUILD_DIR \
+  && cmake \
+           -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+           -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+           -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+           $SOURCE_DIR \
+
+if [ "$*" = "" ] ; then
+    make
+elif [ "$*" = "install" ] ; then
+    cd src && make install
+else
+    echo "Usage: './build.sh' or './build.sh install'"
+fi
